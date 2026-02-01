4ae78bb0dcccec496fe0f4ee81517b9010c46feb
Muhammadmuso <berzloyme@gmail.com>
2026-02-01 13:17:55 +0500

    static fix

--- config/settings.py
+++ config/settings.py
@@ -50,6 +50,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
+    'django.contrib.sessions.middleware.SessionMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -158,6 +164,8 @@ JAZZMIN_UI_TWEAKS = {
     "accent": "accent-light",
     "brand_colour": "navbar-black",
 }
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
