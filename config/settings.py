2cec1ed86d0c884c6d07d877539620c058a0120d
Muhammadmuso <berzloyme@gmail.com>
2026-02-03 16:56:45 +0500

    first commit

--- config/settings.py
+++ config/settings.py
@@ -127,15 +127,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
 
-import os
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+if  DEBUG:
+    STATICFILES_DIRS = [BASE_DIR / 'static']
+else:
+    STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 JAZZMIN_SETTINGS = {
